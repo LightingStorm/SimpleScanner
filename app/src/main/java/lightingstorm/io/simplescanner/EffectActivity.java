73d9b1a743f0c48e4df3fe809feea0aa6488ae9d
phanhaugiang <thienthientan.tin@gmail.com>
2016-12-06 01:22:05 +0700

    Update size + color.xml

--- app/src/main/java/lightingstorm/io/simplescanner/EffectActivity.java
+++ app/src/main/java/lightingstorm/io/simplescanner/EffectActivity.java
@@ -32,52 +32,52 @@ public class EffectActivity extends Activity {
 
     public void btn_original_Click(View view){
         Button btn_grayscale = (Button) this.findViewById(R.id.btn_grayscale);
-        Drawable d1 = getResources().getDrawable(R.drawable.ic_filter_drama_black_36dp);
+        Drawable d1 = getResources().getDrawable(R.drawable.ic_filter_drama_black_24dp);
         btn_grayscale.setCompoundDrawablesWithIntrinsicBounds(null,d1,null,null);
-        btn_grayscale.setTextColor(Color.parseColor("#1A1C27"));
+        btn_grayscale.setTextColor(getResources().getColor(R.color.NearDark));
 
         Button btn_original = (Button) this.findViewById(R.id.btn_original);
-        Drawable d2 = getResources().getDrawable(R.drawable.ic_photo_white_36dp);
+        Drawable d2 = getResources().getDrawable(R.drawable.ic_photo_white_24dp);
         btn_original.setCompoundDrawablesWithIntrinsicBounds(null,d2,null,null);
-        btn_original.setTextColor(Color.parseColor("#F2F2F2"));
+        btn_original.setTextColor(getResources().getColor(R.color.LightWhite));
 
         Button btn_blackwhite = (Button) this.findViewById(R.id.btn_blackwhite);
-        Drawable d3 = getResources().getDrawable(R.drawable.ic_tonality_black_36dp);
+        Drawable d3 = getResources().getDrawable(R.drawable.ic_tonality_black_24dp);
         btn_blackwhite.setCompoundDrawablesWithIntrinsicBounds(null,d3,null,null);
-        btn_blackwhite.setTextColor(Color.parseColor("#1A1C27"));
+        btn_blackwhite.setTextColor(getResources().getColor(R.color.NearDark));
     }
 
     public void btn_grayscale_Click(View view){
         Button btn_grayscale = (Button) this.findViewById(R.id.btn_grayscale);
-        Drawable d = getResources().getDrawable(R.drawable.ic_filter_drama_white_36dp);
+        Drawable d = getResources().getDrawable(R.drawable.ic_filter_drama_white_24dp);
         btn_grayscale.setCompoundDrawablesWithIntrinsicBounds(null,d,null,null);
-        btn_grayscale.setTextColor(Color.parseColor("#F2F2F2"));
+        btn_grayscale.setTextColor(getResources().getColor(R.color.LightWhite));
 
         Button btn_original = (Button) this.findViewById(R.id.btn_original);
-        d = getResources().getDrawable(R.drawable.ic_photo_size_select_actual_black_36dp);
+        d = getResources().getDrawable(R.drawable.ic_photo_size_select_actual_black_24dp);
         btn_original.setCompoundDrawablesWithIntrinsicBounds(null,d,null,null);
-        btn_original.setTextColor(Color.parseColor("#1A1C27"));
+        btn_original.setTextColor(getResources().getColor(R.color.NearDark));
 
         Button btn_blackwhite = (Button) this.findViewById(R.id.btn_blackwhite);
-        d = getResources().getDrawable(R.drawable.ic_tonality_black_36dp);
+        d = getResources().getDrawable(R.drawable.ic_tonality_black_24dp);
         btn_blackwhite.setCompoundDrawablesWithIntrinsicBounds(null,d,null,null);
-        btn_blackwhite.setTextColor(Color.parseColor("#1A1C27"));
+        btn_blackwhite.setTextColor(getResources().getColor(R.color.NearDark));
     }
 
     public void btn_blackwhite_Click(View view){
         Button btn_grayscale = (Button) this.findViewById(R.id.btn_grayscale);
-        Drawable d = getResources().getDrawable(R.drawable.ic_filter_drama_black_36dp);
+        Drawable d = getResources().getDrawable(R.drawable.ic_filter_drama_black_24dp);
         btn_grayscale.setCompoundDrawablesWithIntrinsicBounds(null,d,null,null);
-        btn_grayscale.setTextColor(Color.parseColor("#1A1C27"));
+        btn_grayscale.setTextColor(getResources().getColor(R.color.LightWhite));
 
         Button btn_original = (Button) this.findViewById(R.id.btn_original);
-        d = getResources().getDrawable(R.drawable.ic_photo_size_select_actual_black_36dp);
+        d = getResources().getDrawable(R.drawable.ic_photo_size_select_actual_black_24dp);
         btn_original.setCompoundDrawablesWithIntrinsicBounds(null,d,null,null);
-        btn_original.setTextColor(Color.parseColor("#1A1C27"));
+        btn_original.setTextColor(getResources().getColor(R.color.NearDark));
 
         Button btn_blackwhite = (Button) this.findViewById(R.id.btn_blackwhite);
-        d = getResources().getDrawable(R.drawable.ic_tonality_white_36dp);
+        d = getResources().getDrawable(R.drawable.ic_tonality_white_24dp);
         btn_blackwhite.setCompoundDrawablesWithIntrinsicBounds(null,d,null,null);
-        btn_blackwhite.setTextColor(Color.parseColor("#F2F2F2"));
+        btn_blackwhite.setTextColor(getResources().getColor(R.color.NearDark));
     }
 }
