61a36174b2dc6927a6e5e455772b67e01510f4cc
phanhaugiang <thienthientan.tin@gmail.com>
2016-12-20 22:06:22 +0700

    Done EffectActivity - Tuệ đọc Description
    Bạn tín lưu file out put bên folder : Process -> Var . Bạn tuệ chỉ việc
    lấy danh sách trong đấy load lên và getbackground là xong ( có gì không
    hiểu thì pm tín ) - tks

--- app/src/main/java/lightingstorm/io/simplescanner/EffectActivity.java
+++ app/src/main/java/lightingstorm/io/simplescanner/EffectActivity.java
@@ -2,6 +2,7 @@ package lightingstorm.io.simplescanner;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,8 +11,10 @@ import android.graphics.Color;
 import android.graphics.ColorFilter;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
+import android.graphics.Paint;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,11 +22,21 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ThreadFactory;
 
 import lightingstorm.io.simplescanner.process.ImageViewHelper_Effect;
+import lightingstorm.io.simplescanner.process.Var;
 
 public class EffectActivity extends Activity {
 
+    private boolean first=false;
+    private double scalepercent;
+    private int width=0;
+    private int height=0;
+    private boolean isA4 = false;
     //public ImageView original = (ImageView) this.findViewById(R.id.imageView);
 
     @Override
@@ -33,9 +46,26 @@ public class EffectActivity extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_effect);
 
+        width=((ImageView)this.findViewById(R.id.imageView)).getWidth();
+        height=((ImageView)this.findViewById(R.id.imageView)).getHeight();
+
     }
 
-    public void btn_original_Click(View view){
+    public void btn_original_Click(View view) throws ExecutionException, InterruptedException {
+
+        ImageView iv = (ImageView) this.findViewById(R.id.imageView);
+        iv.buildDrawingCache();
+        Bitmap bmp = iv.getDrawingCache();
+        Bitmap test = (new ImageViewHelper_Effect().execute(bmp).get());
+
+        if (!first)
+        {
+            first = !first;
+            Thread.sleep(500);
+        }
+
+
+
         Button btn_grayscale = (Button) this.findViewById(R.id.btn_grayscale);
         Drawable d1 = getResources().getDrawable(R.drawable.ic_filter_drama_black_24dp);
         btn_grayscale.setCompoundDrawablesWithIntrinsicBounds(null,d1,null,null);
@@ -52,13 +82,26 @@ public class EffectActivity extends Activity {
         btn_blackwhite.setTextColor(getResources().getColor(R.color.BlackRusian));
 
         //Process
-        ImageView iv = (ImageView) this.findViewById(R.id.imageView);
+
         BitmapDrawable ob = new BitmapDrawable(getResources(), (new ImageViewHelper_Effect().convertToOriginal(iv,this)));
         iv.setBackgroundDrawable(ob);
 
     }
 
-    public void btn_grayscale_Click(View view){
+    public void btn_grayscale_Click(View view) throws InterruptedException, ExecutionException {
+
+
+        ImageView iv = (ImageView) this.findViewById(R.id.imageView);
+        iv.buildDrawingCache();
+        Bitmap bmp = iv.getDrawingCache();
+        Bitmap test = (new ImageViewHelper_Effect().execute(bmp).get());
+        if (!first)
+        {
+            first = !first;
+            Thread.sleep(500);
+        }
+
+
         Button btn_grayscale = (Button) this.findViewById(R.id.btn_grayscale);
         Drawable d = getResources().getDrawable(R.drawable.ic_filter_drama_white_24dp);
         btn_grayscale.setCompoundDrawablesWithIntrinsicBounds(null,d,null,null);
@@ -75,13 +118,24 @@ public class EffectActivity extends Activity {
         btn_blackwhite.setTextColor(getResources().getColor(R.color.BlackRusian));
 
         //Process
-        ImageView iv = (ImageView) this.findViewById(R.id.imageView);
+
         BitmapDrawable ob = new BitmapDrawable(getResources(), (new ImageViewHelper_Effect().convertToGrayScale(iv,this)));
         iv.setBackgroundDrawable(ob);
 
     }
 
-    public void btn_blackwhite_Click(View view){
+    public void btn_blackwhite_Click(View view) throws InterruptedException, ExecutionException {
+
+        ImageView iv = (ImageView) this.findViewById(R.id.imageView);
+        iv.buildDrawingCache();
+        Bitmap bmp = iv.getDrawingCache();
+        Bitmap test = (new ImageViewHelper_Effect().execute(bmp).get());
+        if (!first)
+        {
+            first = !first;
+            Thread.sleep(500);
+        }
+
         Button btn_grayscale = (Button) this.findViewById(R.id.btn_grayscale);
         Drawable d = getResources().getDrawable(R.drawable.ic_filter_drama_black_24dp);
         btn_grayscale.setCompoundDrawablesWithIntrinsicBounds(null,d,null,null);
@@ -97,13 +151,20 @@ public class EffectActivity extends Activity {
         btn_blackwhite.setCompoundDrawablesWithIntrinsicBounds(null,d,null,null);
         btn_blackwhite.setTextColor(getResources().getColor(R.color.WhiteSmoke));
 
-        ImageView iv = (ImageView) this.findViewById(R.id.imageView);
+
 
         BitmapDrawable ob = new BitmapDrawable(getResources(),(new ImageViewHelper_Effect()).convertToBlackAndWhite(iv,this));
         iv.setBackgroundDrawable(ob);
     }
 
-    public void btn_rotateleft(View view){
+    public void btn_rotateleft(View view) throws ExecutionException, InterruptedException {
+
+        if (!first)
+        {
+            first = !first;
+            btn_original_Click(view);
+        }
+
         ImageView iv = (ImageView) this.findViewById(R.id.imageView);
         //iv.setImageBitmap(new ImageViewHelper_Effect().rotateImage(iv,this,270));
 
@@ -112,13 +173,69 @@ public class EffectActivity extends Activity {
         iv.setBackgroundDrawable(ob);
     }
 
-    public void btn_rotateright(View view){
+    public void btn_rotateright(View view) throws ExecutionException, InterruptedException {
+        if (!first)
+        {
+            first = !first;
+            btn_original_Click(view);
+        }
         ImageView iv = (ImageView) this.findViewById(R.id.imageView);
         //iv.setImageBitmap(new ImageViewHelper_Effect().rotateImage(iv,this,270));
 
         BitmapDrawable ob = new BitmapDrawable(getResources(),(new ImageViewHelper_Effect()).rotaterightImage(iv,this));
         iv.setBackgroundDrawable(null);
         iv.setBackgroundDrawable(ob);
+
+        return;
     }
+
+    public void scaleToA4(View view){
+        ImageView iv = (ImageView) this.findViewById(R.id.imageView);
+        Button btn_A4 = (Button)this.findViewById(R.id.btn_a4);
+        if (isA4) {
+            isA4 = !isA4;
+            btn_A4.setTextColor(getResources().getColor(R.color.WhiteSmoke));
+            btn_A4.setBackgroundColor(Color.TRANSPARENT);
+
+        }
+        else{
+            isA4 = !isA4;
+            height = (int) ((double)iv.getHeight()/scalepercent);
+            btn_A4.setTextColor(getResources().getColor(R.color.MidnightExpress));
+            btn_A4.setBackgroundColor(getResources().getColor(R.color.WhiteSmoke));
+        }
+    }
+
+    public void btn_back(View view){
+        Intent goToNextActivity = new Intent(getApplicationContext(), CropActivity.class);
+        startActivity(goToNextActivity);
+    }
+
+    public void btn_checkmark(View view){
+        ImageView iv = (ImageView) this.findViewById(R.id.imageView);
+        if (!isA4) {
+            width = iv.getWidth();
+            height = iv.getHeight();
+
+        }
+        else{
+            double scalepercent = (double)iv.getWidth() / 595.0;
+            width = 595;
+            height = (int) ((double)iv.getHeight()/scalepercent);
+        }
+
+        Drawable dr = iv.getBackground();
+        Bitmap b = ((BitmapDrawable)dr).getBitmap();
+        Bitmap bitmapResized = Bitmap.createScaledBitmap(b, width, height, true);
+        dr = new BitmapDrawable(bitmapResized);
+        Var.iv_tranfer = new ImageView(this);
+        Var.iv_tranfer.setBackgroundDrawable(dr);
+        Var.list_iv.add(Var.iv_tranfer);
+        return;
+        //Bitmap scaledown =
+    }
+
+
+
 
 }
--- app/src/main/java/lightingstorm/io/simplescanner/process/ImageViewHelper_Effect.java
+++ app/src/main/java/lightingstorm/io/simplescanner/process/ImageViewHelper_Effect.java
@@ -14,6 +14,9 @@ import android.graphics.drawable.Drawable;
 import android.os.AsyncTask;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import java.util.concurrent.ExecutionException;
 
 import lightingstorm.io.simplescanner.EffectActivity;
 import lightingstorm.io.simplescanner.R;
@@ -22,104 +25,197 @@ import lightingstorm.io.simplescanner.R;
  * Created by Shin on 12/11/2016.
  */
 
-public class ImageViewHelper_Effect {
+public class ImageViewHelper_Effect extends AsyncTask<Bitmap,Bitmap,Bitmap>{
 
     private static ImageViewHelper_Effect instance = new ImageViewHelper_Effect();
     private static int status = -1;
     private static int rotate_left=0;
     private static int rotate_right=0;
-
+    private static Bitmap ori;
+    private static Bitmap gc;
+    private static Bitmap bw;
 
     public ImageViewHelper_Effect(){}
+
+
+    @Override
+    protected Bitmap doInBackground(Bitmap... params) {
+        if (gc==null){
+            Bitmap bmpOriginal =params[0];
+
+            int width, height;
+            height = bmpOriginal.getHeight();
+            width = bmpOriginal.getWidth();
+
+
+
+            Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(bmpGrayscale);
+            Paint paint = new Paint();
+            ColorMatrix cm = new ColorMatrix();
+            cm.setSaturation(0);
+            ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+            paint.setColorFilter(f);
+            c.drawBitmap(bmpOriginal, 0, 0, paint);
+
+
+            //return bmpGrayscale;
+
+            Matrix matrix = new Matrix();
+            matrix.setRotate(-90*rotate_left + 90*rotate_right);
+            status = 1;
+            gc = Bitmap.createBitmap(bmpGrayscale,0,0,width,height,matrix,true);
+        }
+        if (bw==null) {
+            Bitmap src  = params[0];
+            double value = 20;
+
+            int width = src.getWidth();
+            int height = src.getHeight();
+            // create output bitmap
+            Bitmap bmOut = Bitmap.createBitmap(width, height, src.getConfig());
+            // color information
+            int A, R, G, B;
+            int pixel;
+            // get contrast value
+            double contrast = Math.pow((100 + value) / 100, 2);
+
+            // scan through all pixels
+            for (int x = 0; x < width; x++) {
+                for (int y = 0; y < height; y++) {
+                    // get pixel color
+                    pixel = src.getPixel(x, y);
+                    A = Color.alpha(pixel);
+                    // apply filter contrast for every channel R, G, B
+                    R = Color.red(pixel);
+                    R = (int) (((((R / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                    if (R < 0) {
+                        R = 0;
+                    } else if (R > 255) {
+                        R = 255;
+                    }
+
+                    G = Color.red(pixel);
+                    G = (int) (((((G / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                    if (G < 0) {
+                        G = 0;
+                    } else if (G > 255) {
+                        G = 255;
+                    }
+
+                    B = Color.red(pixel);
+                    B = (int) (((((B / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                    if (B < 0) {
+                        B = 0;
+                    } else if (B > 255) {
+                        B = 255;
+                    }
+
+                    // set new pixel color to output bitmap
+                    bmOut.setPixel(x, y, Color.argb(A, R, G, B));
+                }
+            }
+            //return bmOut;
+
+            Matrix matrix = new Matrix();
+            matrix.setRotate(-90 * rotate_left + 90 * rotate_right);
+            status = 2;
+            bw = Bitmap.createBitmap(bmOut, 0, 0, width, height, matrix, true);
+            return bw;
+        }
+        return null;
+    }
+
+
+    public static ImageView original_save ;
 
 
     public ImageViewHelper_Effect getInstance(){
         return instance;
     }
 
-    public static ImageView original_save ;
+    public Bitmap convertToOriginal(ImageView iv,EffectActivity ea){
+        if (ori==null){
+            if (original_save==null){
+                original_save = new ImageView(ea);
+                original_save = iv;
+            }
 
-    public Bitmap convertToGrayScale(ImageView iv,EffectActivity ea){
+            original_save.buildDrawingCache();
+            Bitmap bmpOriginal = original_save.getDrawingCache();
 
-        if (original_save==null){
-            original_save = new ImageView(ea);
-            original_save = iv;
+            int width, height;
+            height = bmpOriginal.getHeight();
+            width = bmpOriginal.getWidth();
+
+            Bitmap out = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(out);
+            Paint paint = new Paint();
+            ColorMatrix cm = new ColorMatrix();
+            cm.setSaturation(1);
+            ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+            paint.setColorFilter(f);
+            c.drawBitmap(bmpOriginal, 0, 0, paint);
+
+            Matrix matrix = new Matrix();;
+            matrix.setRotate(-90*rotate_left + 90*rotate_right);
+            status = 0;
+            ori=Bitmap.createBitmap(out,0,0,width,height,matrix,true);
+            return ori;
         }
-        else {
-            original_save = iv;
+        else{
+            Matrix matrix = new Matrix();
+            matrix.setRotate(-90 * rotate_left + 90 * rotate_right);
+            status = 0;
+            return Bitmap.createBitmap(ori, 0, 0, ori.getWidth(), ori.getHeight(), matrix, true);
         }
 
-
-        iv.buildDrawingCache();
-        Bitmap bmpOriginal = iv.getDrawingCache();
-
-        int width, height;
-        height = bmpOriginal.getHeight();
-        width = bmpOriginal.getWidth();
-
-
-
-        Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(bmpGrayscale);
-        Paint paint = new Paint();
-        ColorMatrix cm = new ColorMatrix();
-        cm.setSaturation(0);
-        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
-        paint.setColorFilter(f);
-        c.drawBitmap(bmpOriginal, 0, 0, paint);
-
-
-        //return bmpGrayscale;
-
-        Matrix matrix = new Matrix();
-        matrix.setRotate(-90*rotate_left + 90*rotate_right);
-        status = 1;
-        return Bitmap.createBitmap(bmpGrayscale,0,0,width,height,matrix,true);
     }
 
-    public Bitmap convertToOriginal(ImageView iv,EffectActivity ea){
-        if (original_save==null){
-            original_save = new ImageView(ea);
-            original_save = iv;
+    public Bitmap convertToGrayScale(ImageView iv,EffectActivity ea){
+        if (gc==null){
+            if (original_save==null){
+                original_save = new ImageView(ea);
+                original_save = iv;
+            }
+            iv.buildDrawingCache();
+            Bitmap bmpOriginal = iv.getDrawingCache();
+
+            int width, height;
+            height = bmpOriginal.getHeight();
+            width = bmpOriginal.getWidth();
+
+
+
+            Bitmap bmpGrayscale = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+            Canvas c = new Canvas(bmpGrayscale);
+            Paint paint = new Paint();
+            ColorMatrix cm = new ColorMatrix();
+            cm.setSaturation(0);
+            ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
+            paint.setColorFilter(f);
+            c.drawBitmap(bmpOriginal, 0, 0, paint);
+
+
+            //return bmpGrayscale;
+
+            Matrix matrix = new Matrix();
+            matrix.setRotate(-90*rotate_left + 90*rotate_right);
+            status = 1;
+            gc = Bitmap.createBitmap(bmpGrayscale,0,0,width,height,matrix,true);
+            return gc;
+
         }
-        else {
-            original_save = iv;
+        else{
+            Matrix matrix = new Matrix();
+            matrix.setRotate(-90 * rotate_left + 90 * rotate_right);
+            status=1;
+            return Bitmap.createBitmap(gc, 0, 0, gc.getWidth(), gc.getHeight(), matrix, true);
         }
-
-        original_save.buildDrawingCache();
-        Bitmap bmpOriginal = original_save.getDrawingCache();
-
-        int width, height;
-        height = bmpOriginal.getHeight();
-        width = bmpOriginal.getWidth();
-
-        Bitmap out = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
-        Canvas c = new Canvas(out);
-        Paint paint = new Paint();
-        ColorMatrix cm = new ColorMatrix();
-        cm.setSaturation(1);
-        ColorMatrixColorFilter f = new ColorMatrixColorFilter(cm);
-        paint.setColorFilter(f);
-        c.drawBitmap(bmpOriginal, 0, 0, paint);
-
-        //return out;
-
-        Matrix matrix = new Matrix();
-        matrix.setRotate(-90*rotate_left + 90*rotate_right);
-        status = 0;
-        return Bitmap.createBitmap(out,0,0,width,height,matrix,true);
     }
 
     public Bitmap convertToBlackAndWhite(ImageView iv , EffectActivity ea){
-
-
-        if (original_save==null){
-            original_save = new ImageView(ea);
-            original_save = iv;
-        }
-        else {
-            original_save = iv;
-        }
+    if (bw==null) {
 
         iv.buildDrawingCache(true);
         Bitmap src = iv.getDrawingCache(true);
@@ -137,26 +233,35 @@ public class ImageViewHelper_Effect {
         double contrast = Math.pow((100 + value) / 100, 2);
 
         // scan through all pixels
-        for(int x = 0; x < width; x++) {
-            for(int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
                 // get pixel color
                 pixel = src.getPixel(x, y);
                 A = Color.alpha(pixel);
                 // apply filter contrast for every channel R, G, B
                 R = Color.red(pixel);
-                R = (int)(((((R / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
-                if(R < 0) { R = 0; }
-                else if(R > 255) { R = 255; }
+                R = (int) (((((R / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                if (R < 0) {
+                    R = 0;
+                } else if (R > 255) {
+                    R = 255;
+                }
 
                 G = Color.red(pixel);
-                G = (int)(((((G / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
-                if(G < 0) { G = 0; }
-                else if(G > 255) { G = 255; }
+                G = (int) (((((G / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                if (G < 0) {
+                    G = 0;
+                } else if (G > 255) {
+                    G = 255;
+                }
 
                 B = Color.red(pixel);
-                B = (int)(((((B / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
-                if(B < 0) { B = 0; }
-                else if(B > 255) { B = 255; }
+                B = (int) (((((B / 255.0) - 0.5) * contrast) + 0.5) * 255.0);
+                if (B < 0) {
+                    B = 0;
+                } else if (B > 255) {
+                    B = 255;
+                }
 
                 // set new pixel color to output bitmap
                 bmOut.setPixel(x, y, Color.argb(A, R, G, B));
@@ -166,75 +271,79 @@ public class ImageViewHelper_Effect {
         //return bmOut;
 
         Matrix matrix = new Matrix();
-        matrix.setRotate(-90*rotate_left + 90*rotate_right);
+        matrix.setRotate(-90 * rotate_left + 90 * rotate_right);
         status = 2;
-        return Bitmap.createBitmap(bmOut,0,0,width,height,matrix,true);
+        bw = Bitmap.createBitmap(bmOut, 0, 0, width, height, matrix, true);
+        return bw;
+        }
+        else{
+            Matrix matrix = new Matrix();
+            matrix.setRotate(-90 * rotate_left + 90 * rotate_right);
+            status=2;
+            return Bitmap.createBitmap(bw, 0, 0, bw.getWidth(), bw.getHeight(), matrix, true);
+        }
     }
 
     public Bitmap rotateleftImage(ImageView iv , EffectActivity ea) {
-
         if (original_save==null){
             original_save = new ImageView(ea);
             original_save = iv;
         }
-        else {
-            original_save = iv;
-        }
 
-
-
-        //iv.buildDrawingCache();
-        //Bitmap src = iv.getDrawingCache();
-        //Matrix matrix = new Matrix();
-        //matrix.setRotate(-90-90*rotate_left);
 
         rotate_left++;
         Bitmap out = null;
         if (status==0){
-            out = convertToOriginal(iv,ea);
+
+            Matrix matrix = new Matrix();
+            matrix.setRotate(-90 * rotate_left + 90 * rotate_right);
+            return Bitmap.createBitmap(ori, 0, 0, ori.getWidth(), ori.getHeight(), matrix, true);
         }
         else if (status==1){
-            out = convertToGrayScale(iv,ea);
+            Matrix matrix = new Matrix();
+            matrix.setRotate(-90 * rotate_left + 90 * rotate_right);
+            return Bitmap.createBitmap(gc, 0, 0, gc.getWidth(), gc.getHeight(), matrix, true);
         }
         else if(status==2){
-            out = convertToBlackAndWhite(iv,ea);
+            Matrix matrix = new Matrix();
+            matrix.setRotate(-90 * rotate_left + 90 * rotate_right);
+            return Bitmap.createBitmap(bw, 0, 0, bw.getWidth(), bw.getHeight(), matrix, true);
         }
         else
             out = convertToOriginal(iv,ea);
         return out;
     }
 
-    public Bitmap rotaterightImage(ImageView iv , EffectActivity ea) {
+    public Bitmap rotaterightImage(ImageView iv , EffectActivity ea) throws ExecutionException, InterruptedException {
 
         if (original_save==null){
             original_save = new ImageView(ea);
             original_save = iv;
         }
-        else {
-            original_save = iv;
-        }
 
-        //iv.buildDrawingCache();
-        //Bitmap src = iv.getDrawingCache();
-        //Matrix matrix = new Matrix();
-        //matrix.setRotate(-90-90*rotate_left);
 
         rotate_right++;
+
         Bitmap out = null;
         if (status==0){
-            out = convertToOriginal(iv,ea);
+            Matrix matrix = new Matrix();
+            matrix.setRotate(-90 * rotate_left + 90 * rotate_right);
+            return Bitmap.createBitmap(ori, 0, 0, ori.getWidth(), ori.getHeight(), matrix, true);
         }
         else if (status==1){
-            out = convertToGrayScale(iv,ea);
+            Matrix matrix = new Matrix();
+            matrix.setRotate(-90 * rotate_left + 90 * rotate_right);
+            return Bitmap.createBitmap(gc, 0, 0, gc.getWidth(), gc.getHeight(), matrix, true);
         }
         else if(status==2){
-            out = convertToBlackAndWhite(iv,ea);
+            Matrix matrix = new Matrix();
+            matrix.setRotate(-90 * rotate_left + 90 * rotate_right);
+            return Bitmap.createBitmap(bw, 0, 0, bw.getWidth(), bw.getHeight(), matrix, true);
         }
         else
             out = convertToOriginal(iv,ea);
         return out;
     }
-
 }
 
 
